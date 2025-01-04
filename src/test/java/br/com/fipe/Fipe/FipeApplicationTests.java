7801ac4df74146fc75469828e51acdeda3b4f944
nadiduno <nadiduno@gmail.com>
2025-01-04 13:56:57 -0300

    Setup project

---
+++ src/test/java/br/com/fipe/Fipe/FipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fipe.Fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
